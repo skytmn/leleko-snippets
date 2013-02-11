b7bc422852af0095d71b6982ba1ca3645b3fa9fd
SergeyL <sergey@leleko.ru>
2013-02-12 00:52:39 +0600

    first variant

---
+++ addCrt.php
@@ -0,0 +1,29 @@
+<?
+$row = $modx->getObject('CdbCartridges', $cli_inn);
+if(!$row) {
+$row = $modx->newObject('CdbCartridges');}
+$row->fromArray(array('inn' => $cli_inn, 
+                      'cartno' => $crt_num, 
+					  'Model' => $crt_model,
+					  'Podrazdelenie' => $crt_unit,
+					  'Zapravka' => $crt_zapravka, 
+					  'Vosstonovlenie' => $crt_vosstanovlenie, 
+					  'ZamenaBarabanaOPC' => $crt_zamenaopc, 
+					  'ZamenaRakelaVB' => $crt_zamenarakela, 
+					  'ZamenaValaZariada' => $crt_zamenavalazar, 
+					  'ZamenaDozLezvia' => $crt_zamenadozlezv, 
+					  'ZamenaMagnitnogoVala' => $crt_zamenamagvala, 
+					  'ZamenaProavlRolika' => $crt_zamenaproavrol, 
+					  'ZamenaChipa' => $crt_zamenachipa, 
+					  'ZamenaCorpusaCartridga' => $crt_zamenacorpkart, 
+					  'ZamenaRolikaPodachiTonera' => $crt_zamenaroltonera,
+					  'Vozvrat' => $crt_vozvrat,
+					  'PoGarantii' => $crt_pogarantii,
+					  'Document' => $crt_document,
+					  'Status' => $crt_status,
+					  'date' => $crt_date, 
+		//			  'time' => $crt_time,
+					  'Manager' => $crt_manager,
+					  'Zapravil' => $crt_zapravil
+					  ));
+$row->save();
